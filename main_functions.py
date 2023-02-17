10f2dd4f9c25d3db860653e0ed2924db6f40e26d
Malek Alsalamat <malsalamat@galahad.cs.uni-kassel.de>
2023-02-17 02:35:13 +0100

    first commit from gal

--- main_functions.py
+++ main_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from prettytable import PrettyTable
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
@@ -10,12 +11,19 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import davies_bouldin_score
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import calinski_harabasz_score
+import struct
+from sklearn.cluster import DBSCAN
+import time
 
 # The following vars are for displaying the end results for each algorithm. See display_results in main_function
 
-list_of_algos, internal_mean_list, external_mean_list, internal_variance_list, external_variance_list = \
-    [], [], [], [], []
+list_of_algos, internal_mean_list, external_mean_list, internal_variance_list, external_variance_list, \
+    internal_min_list, external_min_list, internal_max_list, external_max_list = \
+    [], [], [], [], [], [], [], [], []
 
+df = pd.DataFrame()
+
+time_dic = {}
 
 '''
 # Function to create KMeans models for clustering data
@@ -32,7 +40,7 @@ def create_kmeans_models(k_list, itr_num, data):
     kmeans_models = []
     for k in k_list:
         for i in range(itr_num):
-            model = KMeans(n_clusters=k)
+            model = KMeans(n_clusters=k, init='random', n_init = 1)
             model.fit(data)
             kmeans_models.append(model)
     return kmeans_models
@@ -59,28 +67,15 @@ def create_spectral_clustering_models(k_list, itr_num, data):
     return spectral_clustering_models
 
 
-'''
-Function to create ensemble clustering labels for clustering data
-k_list: List of values for the number of clusters
-itr_num: Number of models to create for each value of k
-ensemble_input: Data to be clustered into k clusters using ensemble clustering
-algo_name: name of the algorithm to use for ensemble clustering (must be one of cspa, mcla, hbgf, nmf)
-Returns a list of ensemble clustering labels with the specified number of clusters and algorithm
-'''
-
-
-def create_ensemble_clustering(k_list, itr_num, ensemble_input, algo_name):
-    ensemble_methods = ('cspa', 'mcla', 'hbgf', 'nmf')
-    assert algo_name.lower() in ensemble_methods, 'The given algorithm name is not in [cspa, mcla, hbgf, nmf], ' \
-                                                  'please use one of these algorithms'
-    ensemble_labels = []
-    for k in k_list:
-        for i in range(itr_num):
-            label = CE.cluster_ensembles(ensemble_input, nclass=k, solver=algo_name.lower())
-            ensemble_labels.append(label)
-    return ensemble_labels
-
-
+def create_dbscan_clustering_models(eps_list, min_samples, data):
+    dbscan_models = []
+    for eps in eps_list:
+        for min_sample in min_samples:
+            model = DBSCAN(eps, min_sample)
+            model.fit(data)
+            dbscan_models.append(model)
+    return dbscan_models
+            
 '''
 Function to add a Species column to a dataframe based on a target column
 df: Dataframe to add the Species column to
@@ -90,9 +85,9 @@ as 'cluster_' followed by the value of the target column.
 '''
 
 
-def define_species(df, target_column):
-    species = ["cluster_" + str(val) for val in target_column]
-    df['Species'] = species
+# def define_species(df, target_column):
+#     species = ["cluster_" + str(val) for val in target_column]
+#     df['Species'] = species
 
 
 '''
@@ -108,7 +103,7 @@ def calculate_external_validation(true_labels, predicted_labels):
     nmf_score = normalized_mutual_info_score(true_labels, predicted_labels, average_method='geometric')
     ar_score = adjusted_rand_score(true_labels, predicted_labels)
     j_score = jaccard_score(true_labels, predicted_labels, average='weighted')
-    return [round(nmf_score, 3), round(ar_score, 3), round(j_score, 3)]
+    return [nmf_score, ar_score, j_score]
 
 
 '''
@@ -119,29 +114,17 @@ Returns a list of internal validation metrics (Davies-Bouldin score, silhouette 
 '''
 
 
-def calculate_internal_validation(actual_data, predicted_labels):
-    reduced_data = PCA(n_components=2).fit_transform(actual_data)
+def calculate_internal_validation(actual_data, predicted_labels, algo_name):
+    if algo_name.lower() == 'dbscan':
+        reduced_data = actual_data
+    else:
+        reduced_data = PCA(n_components=2).fit_transform(actual_data)
     db_score = davies_bouldin_score(reduced_data, predicted_labels)
     sh_score = silhouette_score(reduced_data, predicted_labels)
     ch_score = calinski_harabasz_score(reduced_data, predicted_labels)
-    return [round(db_score, 3), round(sh_score, 3), round(ch_score, 3)]
+    return [db_score, sh_score, ch_score]
 
 
-'''
-Function to calculate the mean of the validation scores
-validation_results: List of results obtained from calculating internal or external validation
-Returns a list of the mean values for each validation score
-'''
-
-
-def calculate_mean_of_validation(validation_results):
-    first_score, second_score, third_score, length = 0, 0, 0, 0
-    for [x, y, z] in validation_results:
-        first_score += x  # x is either davies_bouldin_score or  normalized_mutual_info_score
-        second_score += y  # y is either silhouette_score or adjusted_rand_score
-        third_score += z  # z is either calinski_harabasz_score or jaccard_score
-        length += 1  # length is the size of validation_results
-    return [round(first_score / length, 3), round(second_score / length, 3), round(third_score / length, 3)]
 
 
 '''
@@ -174,10 +157,10 @@ Returns a list of internal validation metrics for each model in the form of [db_
 '''
 
 
-def calculate_internal_validation_for_all_labels(actual_data, predicted_labels):
+def calculate_internal_validation_for_all_labels(actual_data, predicted_labels, algo_name):
     validation_results = []
     for label in predicted_labels:
-        validation_results.append(calculate_internal_validation(actual_data, label))
+        validation_results.append(calculate_internal_validation(actual_data, label, algo_name))
     return validation_results
 
 
@@ -196,96 +179,20 @@ def calculate_external_validation_for_all_labels(true_labels, predicted_labels):
     return validation_results
 
 
-'''
-Calculates the variance of the validation scores in the validation_results list.    
-Parameters:
-validation_results (List): List of results obtained from calculating internal or external validation   
-mean (List): List of mean values for the given validation_results 
-Returns:
-List: A list of the variance values for each validation score
-'''
 
 
-def calculate_variance_of_validation(validation_results, mean):
-    first_sum_square, second_sum_square, third_sum_square, length = 0, 0, 0, 0
-    for [x, y, z] in validation_results:
-        first_sum_square += (x - mean[0]) ** 2
-        second_sum_square += (y - mean[1]) ** 2
-        third_sum_square += (z - mean[2]) ** 2
-        length += 1
-    return [round(first_sum_square / length, 3), round(second_sum_square / length, 3),
-            round(third_sum_square / length, 3)]
 
 
-"""
-This function use the following - list of algorithm names, list of k values, number of iterations, 
-lists of mean values for internal and external validation, and lists of variance values for internal 
-and external validation.
-The function returns a table with the input values organized in a readable format, displaying the algorithm names, 
-k values, iteration number, and mean and variance values for both internal and external validation.
-"""
 
 
-def display_results(k_list, itr_num):
-    x = PrettyTable()
-    x.field_names = ["", *list_of_algos]
-    x.add_row(["k_list", *k_list * len(list_of_algos)])
-    x.add_row(["itr_num", *[itr_num] * len(list_of_algos)])
-    internal_mean_fields = ["Internal_Mean_DB_Score", "Internal_Mean_Sil_Score", "Internal_Mean_CH_Score"]
-    for i in range(3):
-        row = [internal_mean_fields[i]]
-        for j in range(len(list_of_algos)):
-            row.append(internal_mean_list[j][i])
-        x.add_row(row)
-    external_mean_fields = ["External_Mean_NMI_Score", "External_Mean_ARI_Score", "External_Mean_J_Score"]
-    for i in range(3):
-        row = [external_mean_fields[i]]
-        for j in range(len(list_of_algos)):
-            row.append(external_mean_list[j][i])
-        x.add_row(row)
-    internal_variance_fields = ["Internal_Variance_DB_Score", "Internal_Variance_Sil_Score",
-                                "Internal_Variance_CH_Score"]
-    for i in range(3):
-        row = [internal_variance_fields[i]]
-        for j in range(len(list_of_algos)):
-            row.append(internal_variance_list[j][i])
-        x.add_row(row)
-    external_variance_fields = ["External_Variance_NMI_Score", "External_Variance_ARI_Score",
-                                "External_Variance_J_Score"]
-    for i in range(3):
-        row = [external_variance_fields[i]]
-        for j in range(len(list_of_algos)):
-            row.append(external_variance_list[j][i])
-        x.add_row(row)
-    print(x)
-
-
-"""
-Adds the results of a single algorithm to the lists of results.
-Args:
-    - algo_name (str): name of the algorithm
-    - internal_mean (list): list of internal mean scores for each evaluation metric
-    - external_mean (list): list of external mean scores for each evaluation metric
-    - internal_variance (list): list of internal variance scores for each evaluation metric
-    - external_variance (list): list of external variance scores for each evaluation metric
-Returns:
-    None (updates the global lists)
-"""
-
-
-def add_results(algo_name, internal_mean, external_mean, internal_variance, external_variance):
-    list_of_algos.append(algo_name)
-    internal_mean_list.append(internal_mean)
-    external_mean_list.append(external_mean)
-    internal_variance_list.append(internal_variance)
-    external_variance_list.append(external_variance)
 
 
 def clear_results():
-    global list_of_algos, internal_mean_list, external_mean_list, internal_variance_list, external_variance_list
+    global time_dic, df, list_of_algos, internal_mean_list, external_mean_list, internal_variance_list, external_variance_list
     list_of_algos, internal_mean_list, external_mean_list, internal_variance_list, external_variance_list = \
         [], [], [], [], []
-
+    df = pd.DataFrame()
+    time_dic = {}
 
 """
 This function performs the internal and external validation calculation for a given clustering algorithm and 
@@ -302,17 +209,26 @@ Outputs:
 
 
 def add_validation_results(data, target, labels, algo_name):
-    internal = calculate_internal_validation_for_all_labels(data, labels)
-    internal_mean = calculate_mean_of_validation(internal)
-    internal_variance = calculate_variance_of_validation(internal, internal_mean)
-
+    global df
+    
+    internal = calculate_internal_validation_for_all_labels(data, labels, algo_name)
     external = calculate_external_validation_for_all_labels(target, labels)
-    external_mean = calculate_mean_of_validation(external)
-    external_variance = calculate_variance_of_validation(external, external_mean)
 
-    add_results(algo_name, internal_mean, external_mean, internal_variance, external_variance)
+    internal_scores = list(zip(*internal))
+    external_scores = list(zip(*external))
+    
+    db_score = list(internal_scores[0])
+    sh_score = list(internal_scores[1])
+    ch_score = list(internal_scores[2])
+    nmf_score = list(external_scores[0])
+    ar_score = list(external_scores[1])
+    j_score = list(external_scores[2])
 
 
+    df = df.append({'Algorithm': algo_name, 'DB_Score': db_score, 'SH_Score': sh_score, 'CH_Score': ch_score,
+                   'NMF_Score': nmf_score, 'AR_Score': ar_score, 'J_Score': j_score}, ignore_index=True)
+
+    
 '''
 Function for generating ensemble clustering models
 Inputs:
@@ -326,11 +242,124 @@ Output: None (results are stored in global variables for validation purposes)
 '''
 
 
-def perform_ensemble_clustering(base_models, k_list, itr_num, data, target):
-    ensemble_methods = ['NMF', 'CSPA', 'MCLA', 'HBGF']
+def perform_ensemble_clustering(base_clustering, k_list, itr_num, data, target):
+    ensemble_methods = ['nmf', 'cspa', 'mcla', 'hbgf']
     for method in ensemble_methods:
-        ensemble_input = modify_models_to_valid_ensemble_input(base_models)
-        ensemble_labels = create_ensemble_clustering(k_list, itr_num, ensemble_input, method)
+        ensemble_labels = create_ensemble_clustering(k_list, itr_num, base_clustering, method, data)
         add_validation_results(data, target, ensemble_labels, method)
 
-#%%
+
+'''
+Function to create ensemble clustering labels for clustering data
+k_list: List of values for the number of clusters
+itr_num: Number of models to create for each value of k
+ensemble_input: Data to be clustered into k clusters using ensemble clustering
+algo_name: name of the algorithm to use for ensemble clustering (must be one of cspa, mcla, hbgf, nmf)
+Returns a list of ensemble clustering labels with the specified number of clusters and algorithm
+'''
+
+
+def create_ensemble_clustering(k_list, itr_num, base_clustering, algo_name, data):
+    global time_dic
+    ensemble_methods = ('cspa', 'mcla', 'hbgf', 'nmf')
+    assert algo_name.lower() in ensemble_methods, 'The given algorithm name is not in [cspa, mcla, hbgf, nmf], ' \
+                                                  'please use one of these algorithms'
+    ensemble_labels = []
+    for k in k_list:
+        for i in range(itr_num):
+            if base_clustering.lower() == 'kmeans':
+                k_means_models = create_kmeans_models(k_list, itr_num, data)
+                ensemble_input = modify_models_to_valid_ensemble_input(k_means_models)
+            else:
+                spectral_models = create_spectral_clustering_models(k_list, itr_num, data)
+                ensemble_input = modify_models_to_valid_ensemble_input(spectral_models)
+            start_time = time.time()
+            label = CE.cluster_ensembles(ensemble_input, nclass=k, solver=algo_name)
+            end_time = time.time()
+            time_taken = end_time - start_time
+            
+            if algo_name in time_dic:
+                time_dic[algo_name].append(time_taken)
+            else:
+                time_dic[algo_name] = [time_taken]
+            ensemble_labels.append(label)
+    return ensemble_labels
+
+
+
+
+
+
+
+
+def create_ensemble_clustering_with_dbscan(k_list, itr_num, dbscan_models, data, target):
+    ensemble_methods = ['nmf', 'cspa', 'mcla', 'hbgf']
+    ensemble_labels = []
+    for method in ensemble_methods:
+        for k in k_list:
+            for i in range(itr_num):
+                ensemble_input = modify_models_to_valid_ensemble_input(dbscan_models)
+                label = CE.cluster_ensembles(ensemble_input, nclass=k, solver=method)
+                ensemble_labels.append(label)
+        add_validation_results(data, target, ensemble_labels, method)        
+        ensemble_labels = []
+    
+
+    
+    
+    
+    
+    
+    
+    
+        
+def get_df():
+    global df
+    return df
+
+
+def get_time_dic():
+    global time_dic
+    return time_dic
+
+
+
+
+
+def display_results(df):
+    validation_techniques = ['DB_Score', 'SH_Score', 'CH_Score', 'NMF_Score', 'AR_Score', 'J_Score']
+    algo_names = list(df['Algorithm'])
+    all_values = []
+    for i in range(len(algo_names)):
+        values_of_alog = []
+        for j in validation_techniques:
+            score = df.at[i, j]
+            min_ = np.min(score)
+            max_ = np.max(score)
+            mean = np.mean(score)
+            var = np.var(score)
+            values_of_alog.extend([min_, max_, mean, var])
+        all_values.append(values_of_alog)
+        values_of_algo = []
+    df = pd.DataFrame(all_values,
+                          index=pd.Index(algo_names, name='Algorithm name'),
+                          columns=pd.MultiIndex.from_product([validation_techniques,['Min', 'Max', 'Mean', 'Var']], names=['Validation technique:', '']))
+    df = df.round(3)
+    return df.style
+  
+    
+    
+    
+    
+    
+    
+    
+    
+def read_idx(filename):
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+
+
+# %%
